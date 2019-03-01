593ac53eb350f484a86827c1315513301db7c30b
Øyvind Johannessen <oyvindjohannessen@pprotonmail.com>
2019-03-01 15:07:43 +0100

    Oppdatert LinkedList add() remove()

--- assignment2/src/friend_app/linked_list/Node.java
+++ assignment2/src/friend_app/linked_list/Node.java
@@ -8,15 +8,13 @@ package friend_app.linked_list;
 public class Node<T> {
     protected T dataItem; // The entry in a list
     protected Node next; // Links to the next node who contains another data and possible next node
-    protected int position;
 
     /**
      * Constructor for node class. Set's the dataItem to dataItem parameter
      * @param dataItem the item to which the Node should hold
      */
-    protected Node(T dataItem, Node next, int position) {
+    protected Node(T dataItem, Node next) {
         this.dataItem = dataItem;
         this.next = next;
-        this.position = position;
     }
 }
